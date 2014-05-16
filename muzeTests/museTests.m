d3e722c69b6ed49a70477bf1465e3a71d94e4986
Etay Luz <etayluz@gmail.com>
2014-05-15 21:58:02 -0400

    Save work

---
+++ muzeTests/museTests.m
@@ -0,0 +1,34 @@
+//
+//  mPloyTests.m
+//  mPloyTests
+//
+//  Created by Etay Luz on 4/2/14.
+//  Copyright (c) 2014 com.bluewhaleapps.scoutpeek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mPloyTests : XCTestCase
+
+@end
+
+@implementation mPloyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
