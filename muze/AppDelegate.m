b59b002a9b6d420b34e39c916f8f0b82182e48f7
Etay Luz <etayluz@gmail.com>
2014-05-18 00:29:48 -0400

    Save work

--- muze/AppDelegate.m
+++ muze/AppDelegate.m
@@ -27,21 +27,18 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    //Player *player = [Player player];
-    //if (player.isMovieLiked)
-        //player.footer.image = [UIImage imageNamed:@"buttomBarSelectedPlay.png"];
-    //else
-       // player.footer.image = [UIImage imageNamed:@"buttomBarPlay.png"];
-
     
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     Player *player = [Player player];
+    player.pauseImage.hidden = YES;
+    player.pauseButton.enabled = NO;
+    player.playImage.hidden = NO;
+    player.playButton.enabled = YES;
     [player showMenu];
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
