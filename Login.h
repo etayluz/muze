e1bf8fa60e3f218c0aed5afe62ba81e89b9353c7
Etay Luz <etayluz@gmail.com>
2014-05-22 16:24:14 -0400

    Save work

---
+++ Login.h
@@ -0,0 +1,13 @@
+//
+//  Login.h
+//  muze
+//
+//  Created by Etay Luz on 5/22/14.
+//  Copyright (c) 2014 com.luzsoft.muzeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Login : UIViewController
+
+@end
---
+++ Login.m
@@ -0,0 +1,31 @@
+//
+//  Login.m
+//  muze
+//
+//  Created by Etay Luz on 5/22/14.
+//  Copyright (c) 2014 com.luzsoft.muzeme. All rights reserved.
+//
+
+#import "Login.h"
+
+@interface Login ()
+
+@end
+
+@implementation Login
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
