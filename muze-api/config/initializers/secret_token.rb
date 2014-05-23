5e8c4deb875fe5ce5a5b67a6543b06da5fa45005
Etay Luz <etayluz@gmail.com>
2014-05-22 22:18:50 -0400

    Save work

---
+++ muze-api/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuzeApi::Application.config.secret_key_base = '99fd950595188019129408a47d2864f24390293d009586b5b5624072bc60f0638b9650d8eb11010b72926d0792c223dd79a4cb441730263551c83294bf9d424e'
---
+++ muze-api/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MuzeApi::Application.config.session_store :cookie_store, key: '_muze-api_session'
