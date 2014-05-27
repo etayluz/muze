ca19e2818086d388c521994b8cd5dc8eca420b26
Etay Luz <etayluz@gmail.com>
2014-05-27 03:08:36 -0400

    Save work

--- muze-api/app/controllers/users_controller.rb
+++ muze-api/app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
 
 	def create
-		puts "create"
-		@tweet = {"tweet"=>1}
-		render json: @tweet
-		# respond_to do |format|
-		# 	format.json { render json: @tweet }
-		# end
+		@user = User.create({"uuid"=>params[:uuid]})
+		render json: @user
+		# puts "create"
+		# @tweet = {"tweet"=>1}
+		# render json: @tweet
 	end
 end
